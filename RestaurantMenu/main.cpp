25c946cb962b9e8aea1e73b8232789f005e96ba6
borisssang <borisssang@gmail.com>
2016-11-10 20:11:14 +0200

    Initial Commit

---
+++ RestaurantMenu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RestaurantMenu
+//
+//  Created by Boris Angelov on 11/10/16.
+//  Copyright © 2016 Boris Angelov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
