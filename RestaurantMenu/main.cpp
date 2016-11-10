db4876a2f6c00952b03667becaa3ac9945644c4b
borisssang <borisssang@gmail.com>
2016-11-10 20:13:26 +0200

    babysteps
    hello

--- RestaurantMenu/main.cpp
+++ RestaurantMenu/main.cpp
@@ -7,9 +7,7 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout<<"success"<<endl;
 }
